3268c0eeaec4930d1f0ad8c2464d0df34b37c759
Christine Hoang <christinehoangt@gmail.com>
2024-02-18 02:26:45 -0600

    blog code merged

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+import './Post.css';
+
+export default function Post() {
+  const {title, content} = useLoaderData();
+
+  return (
+    <div className="Post">
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import App from './App.jsx';
+import Home from './Components/Home/Home.jsx';
+import TopicList from './Components/TopicList/TopicList.jsx';
+import Post from './Components/Post/Post.jsx';
+import postData from './data/post_data.json';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path = "vite-ghpages/" element={<App />}>
+    <Route index element={<Home/>}/>
+    <Route
+
+    path = "vite-ghpages/topics"
+    element={<TopicList/>}
+    loader={()=> postData}/>
+
+    <Route
+
+    path = "vite-ghpages/post/:id"
+    element={<Post/>}
+    loader={({params})=>{
+      const post = postData.find((item)=>item.id === +params.id)
+      if (post){
+        return post;
+      }else{
+        return {title: 'Post Not Found'};
+      }
+    }}
+    />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
