9fca6afe78fac93ad292d1aceb697e1c757e7e2c
Christine Hoang <christinehoangt@gmail.com>
2024-02-19 15:46:13 -0600

    mobile design only

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="Home">
       <h1>Welcome to the DevMountain tech blog!</h1>
       <h2>Head over to the topic browser to see all the latest posts!</h2>
+      {/* <img src = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg" 
+            class="text-center sm:text-left"/> */}
     </div>
   );
 }
