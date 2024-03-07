73d26d6ffea51c9f2ada5168c5f5ced43b2b4bed
Christine Hoang <christinehoangt@gmail.com>
2024-03-06 23:29:44 -0600

    saving changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -24,12 +24,11 @@ const Home = () =>{
       <img src = {Hero6} alt = "deserthero" className = "w-[100%] h-[100%] relative z-[5]"/>
 
       <div className = "absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient">
-        </div>
+        </div> 
         <div className = "absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient">
         </div>
         <div className = "absolute z-[0] w-[20%] h-[40%] right-20 bottom-20 orange__gradient">
         </div>
-
     </section>
   );
 }
