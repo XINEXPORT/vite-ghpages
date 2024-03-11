1cdf4eff6ed8767028eca485454107c54a0c96af
Joe Aguado <aguado.joe@gmail.com>
2024-03-11 10:37:37 -0500

    about images and contact modified -J

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,14 +18,14 @@ const Home = () =>{
         <div className='text-[37px] md:text-[76px] tracking-widest leading-none'>& Joe Aguado</div>
       </div>
 
-      <img src = {Hero6} alt = "deserthero" className = " relative z-[5]"/>
+      <div className={`flex-1 flex ${styles.flexCenter} relative`}>
 
-      {/* <div className = "absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient">
-      </div> 
-      <div className = "absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient">
+      <img src = {Hero6} alt = "deserthero" className = "w-[100%] h-[100%] relative z-[5]"/>
       </div>
-      <div className = "absolute z-[0] w-[20%] h-[40%] right-20 bottom-20 orange__gradient">
-      </div> */}
+
+      {/* <div className = "absolute z-[0] w-[20%] h-[20%] top-40 pink__gradient"/> */}
+      {/* <div className = "absolute z-[0] w-[40%] h-[30%] rounded-full right-5 yellow__gradient" /> */}
+      {/* <div className = "absolute z-[0] w-[20%] h-[40%] right-20 bottom-20 orange__gradient" /> */}
       </div>
     </section>
   );
