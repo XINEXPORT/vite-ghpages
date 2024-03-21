1ae6d7bbc2c39d67580e1abbb9676db329230f5f
Christine Hoang <christinehoangt@gmail.com>
2024-03-20 22:58:18 -0500

    wac24 added

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -6,13 +6,13 @@ export default function Post() {
   const {title, content, content1, content2, content3, content4} = useLoaderData();
 
   return (
-    <div className="text-tccred">
+    <div className="">
       <h1 className = {`${styles.heading1} flex-1 font-helvetica font-semibold ss:text-[30px] text-[30px] text-tccred ss:leading-[100px] leading-[75px]`}>{title}</h1>
-      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 `}>{content}</p>
-      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 `}>{content1}</p>
-      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 `}>{content2}</p>
-      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 `}>{content3}</p>
-      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 `}>{content4}</p>
+      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 text-tccred`}>{content}</p>
+      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 text-tccred`}>{content1}</p>
+      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 text-tccred`}>{content2}</p>
+      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 text-tccred`}>{content3}</p>
+      <p className =  {`${styles.paragraph} ${styles.marginX} ${styles.marginY} max-w-[470px], mt-5 text-tccred`}>{content4}</p>
 
     </div>
   );
