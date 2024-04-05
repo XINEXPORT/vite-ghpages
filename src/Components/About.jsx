2b688ae0adc616368d9a65c1e6041b6166d1ed58
Christine Hoang <christinehoangt@gmail.com>
2024-04-04 19:38:04 -0500

    added google doc About content into About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -38,6 +38,25 @@ console.log(aboutData)
   return (
     <section className={` flex flex-col items-center sm:h-screen`}>
       <h1 className={`font-rye text-[40px] tracking-widest`}>About</h1>
+      <p>The origin of the Texas Coding Club started as a collaboration project between web development students, Joe Aguado and Christine Hoang. After completing their education in Dallas Texas, they attended a local study group and started a Github project in an attempt to strengthen their technical and team-working skills. </p>
+      <p>Joe and Christine would like to introduce the Texas Coding Club website as an open source project to the local development community. Their goal is to allow other Texas-based developers to contribute content to the blog, code for the website and to also create a supportive website and community for local developers. </p>
+      <p>If you’d like to see this project take on a different look, have an added feature or help it get a functioning back-end, let us know! The codebase is on a Github repository that we can share. The intent for this project is to serve as a sandbox for us wanting to try out new skills or technologies as we learn them and see it to full production.</p>
+      <p>We would like to say a big thanks to our study group and peers on helping us spring-board our ideas and helping us publish our site onto the web.</p>
+        <h2>The stack we used to build this project:</h2>
+        <ul>
+          <li>Vite</li>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>TailWind CSS</li>
+          <li>Dreamhost</li>
+        </ul>  
+        <h2>Work Management Tools:</h2>
+        <ul>
+          <li>Figma</li>
+          <li>Canva</li>
+          <li>Google Docs</li>
+        </ul>
+
     <div className={`${styles.padding} ss:bg-primary flex flex-col md:flex-row justify-center items-center text-center`}>
 
       {aboutData.map((person) => (
